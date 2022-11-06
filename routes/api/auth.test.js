380df8c6ccae1bf9f47275b8f235c9b86f159cdd
DribDrob <drobushenko@gmail.com>
2022-11-06 18:58:29 +0200

    add signIn test

--- routes/api/auth.test.js
+++ routes/api/auth.test.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
-const request = require("supertest");
-const bcrypt = require("bcryptjs");
 require("dotenv").config();
-
 const app = require("../../app");
 const { User } = require("../../models/user");
+const request = require("supertest");
 
 const { DB_TEST_HOST } = process.env;
 
-describe("test auth routes", () => {
+describe("test auth routes", function () {
   let server;
   beforeAll(() => (server = app.listen(3000)));
+
   afterAll(() => server.close());
 
   beforeEach((done) => {
@@ -18,42 +17,43 @@ describe("test auth routes", () => {
   });
 
   afterEach((done) => {
-    mongoose.connection.db.dropCollection(() => {
-      mongoose.connection.close(() => done());
-    });
+    mongoose.connection.close(() => done());
+    // mongoose.connection.db.dropCollection(() => {
+    //   mongoose.connection.close(() => done());
+    // });
   });
 
-  test("test login route", async () => {
-    const hashPassword = await bcrypt.hash("123456", 10);
+  test("test signUp", async () => {
     const newUser = {
-      email: "test@gmail.com",
-      password: hashPassword,
+      email: "testmail@gmail.com",
+      password: "testPWD",
     };
 
-    const user = await User.create(newUser);
-
+    const responseSignUp = await request(app)
+      .post("/api/users/signup")
+      .send(newUser);
     /*
     відповідь повина мати статус-код 200
     у відповіді повинен повертатися токен
     у відповіді повинен повертатися об'єкт user з 2 полями email и subscription з типом даних String
         */
-    const loginUser = {
-      email: "test@gmail.com",
-      password: "123456",
-    };
+    expect(responseSignUp.body.user.email).toBe(newUser.email);
+    expect(responseSignUp.statusCode).toBe(201);
 
-    const response = await request(app)
+    const responseSignIn = await request(app)
       .post("/api/users/login")
-      .send(loginUser);
-    console.log(response);
-    expect(response.statusCode).toBe(200);
-    const { body } = response;
+      .send(newUser);
+
+    expect(responseSignIn.statusCode).toBe(200);
+    expect(typeof responseSignIn.body.user).toBe("object");
+    expect(responseSignIn.body.user.email).toBe(newUser.email);
+
+    const user = await User.findOne({ email: newUser.email });
+
+    const { body } = responseSignIn;
     expect(body.token).toBeTruthy();
     const { token } = await User.findById(user._id);
     expect(body.token).toBe(token);
-    // expect(response.user).objectContaining({
-    //   email: expect.any(String),
-    //   subscription: expect.any(String),
-    // });
+    expect(responseSignIn.body.user.subscription).toBe(user.subscription);
   });
 });
